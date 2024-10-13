be6cdc5b08f9198dbfb0616d2883f9b4d74e6207
Yunsu <shin4895@addd.co.kr>
2024-10-13 23:27:21 +0900

    chore : 필요없는 로그 삭제

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -10,7 +10,6 @@ export class AuthController {
     @Body("email") email: string,
     @Body("password") password: string
   ) {
-    console.log("Login 호출");
     const user = await this.authService.validateUser(email, password);
     return this.authService.login(user);
   }
