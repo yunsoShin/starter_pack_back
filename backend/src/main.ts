b87b7c89c6e1bdd93497b301b69dd522599b9128
Yunsu <shin4895@addd.co.kr>
2024-10-16 00:30:07 +0900

    add : 앱시작시 마이그레이션 설정 검증

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as cookieParser from "cookie-parser";
 import * as dotenv from "dotenv";
+import { AppDataSource } from "../typeorm.config";
 
 async function bootstrap() {
   dotenv.config();
+  await AppDataSource.initialize();
+  await AppDataSource.runMigrations();
+
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser()); // CSRF 토큰을 위해 쿠키 파서 사용
   app.enableCors();
